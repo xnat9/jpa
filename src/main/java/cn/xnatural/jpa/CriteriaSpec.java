f6cc261baaf63cd610878cb35a421339298141b5
hubert <xnatural@msn.cn>
2021-10-11 11:22:51 +0800

    FunctionalInterface CriteriaSpec

--- src/main/java/cn/xnatural/jpa/CriteriaSpec.java
+++ src/main/java/cn/xnatural/jpa/CriteriaSpec.java
@@ -8,13 +8,14 @@ import javax.persistence.criteria.Root;
  * Criteria 查询 spec
  * @param <E>
  */
+@FunctionalInterface
 public interface CriteriaSpec<E> {
     /**
      * 查询条件构建
-     * @param root
-     * @param query
-     * @param cb
-     * @return
+     * @param root Root
+     * @param query CriteriaQuery
+     * @param cb CriteriaBuilder
+     * @return E
      */
     E toPredicate(Root<E> root, CriteriaQuery<?> query, CriteriaBuilder cb);
 }
